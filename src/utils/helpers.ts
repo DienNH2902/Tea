d0eda38520b76ed6e87f30912ed61dccf1ebd6e1
DienNH2902 <diennguyenhoang2902@gmail.com>
2026-09-19 10:24:11 +0700

    fix: password

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,10 +3,12 @@ import * as bcrypt from 'bcrypt';
 export class HashUtil {
   private static readonly SALT_ROUNDS = 10;
 
+  // Hash mật khẩu để bảo mật
   static async hash(password: string): Promise<string> {
     return bcrypt.hash(password, this.SALT_ROUNDS);
   }
 
+  // So sánh mật khẩu đã mã hóa với mật khẩu user nhập
   static async compare(
     plainPassword: string,
     hashedPassword: string,
