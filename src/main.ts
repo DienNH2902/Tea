8034bb167f5d2836955d7727af0f560629465ada
DienNH2902 <diennguyenhoang2902@gmail.com>
2026-09-21 08:46:16 +0700

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,29 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CẤU HÌNH CORS CHUẨN CHO ĐỒ ÁN / DỰ ÁN CHẠY COOKIE
+  app.enableCors({
+    // 1. Chỉ định chính xác địa chỉ của Frontend được phép gọi tới
+    // origin: 'http://localhost:5173', // Sửa lại đúng Port mà FE của bạn đang chạy (3000, 5173, v.v.)
+    origin: [
+      'http://localhost:3000',
+      'https://horse-racing.io.vn', // Điền domain chính thức của Frontend vào đây
+      'https://goldenhoof-fe.vercel.app',
+      'null',
+      'http://localhost:5500', // ← Live Server port mặc định
+      'http://127.0.0.1:5500',
+    ],
+
+    // 2. Cho phép các phương thức HTTP nào được quyền tác động vào BE
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+
+    // 3. Cho phép FE gửi kèm Cookie hoặc Header xác thực (Rất quan trọng khi dùng Cookie)
+    credentials: true,
+
+    // 4. Các Header mà FE được phép gửi lên
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With, Accept',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Tea Service') // Tiêu đề hiển thị trên Swagger UI
     .setDescription('Tea Service API Documentation') // Mô tả chi tiết hơn về API
